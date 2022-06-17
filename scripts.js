1f607d96c612d10fa09abb3b45fa21302a8d1c37
YungGeebs <jonathanboulton1@gmail.com>
2022-06-17 15:27:59 -0400

    add more info to introduction and linked to NASA API

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ function myFunction() {
 }
 // Astrology picture of the day 
 const APOD = document.getElementById('APOD');
-fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+fetch('https://api.nasa.gov/planetary/apod?api_key=Q16WKNVz9PgTSPrqI3kuQ7uc5hwQ0Rpvy5tNE1Ot')
   .then(function(response) {
     return response.json();
   })
