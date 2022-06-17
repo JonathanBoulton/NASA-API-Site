93f42861c9decab44ad2748457e52338abc1a4a3
YungGeebs <jonathanboulton1@gmail.com>
2022-06-16 20:14:52 -0400

    made header sticky

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
+window.onscroll = function() {myFunction()};
+let header = document.getElementById("myHeader");
+let sticky = header.offsetTop;
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
 const img = document.querySelector('img');
 fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
   .then(function(response) {
